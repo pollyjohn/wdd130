40de1ae269490790a2fa480a61a9786a8c510d66
pollyjohn <joaotv16@gmail.com>
2025-07-05 03:55:54 -0300

    "staeting index.html"

--- wwr/file.js
+++
@@ -1,7 +0,0 @@
-function setMainMarginTop() {
-    const headerHeight = document.getElementById('header').offsetHeight;
-    document.querySelector('main').style.marginToptop = '${headerHeight}px';
-}
-  
-window.onload = setMainMarginTop;
-  
